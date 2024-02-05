ce4dbbd420d55ceedf4b6cd617f7f72ec45a499b
Mickael Rebeau <rebeau.mickael@gmail.com>
2024-02-05 23:55:19 +0100

    🛠️ build: update gateway module

--- src/gateway/gateway.module.ts
+++ src/gateway/gateway.module.ts
@@ -7,21 +7,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ChatsModule } from 'src/chats/chats.module';
 import { AuthModule } from 'src/auth/auth.module';
 import { UserService } from 'src/user/user.service';
-import { GoogleUserService } from 'src/google-user/google-user.service';
-import { GithubUserService } from 'src/github-user/github-user.service';
 import { AvatarService } from 'src/avatar/avatar.service';
 import { JwtService } from '@nestjs/jwt';
 import { User } from 'src/user/model/user.entity';
-import { GoogleUser } from 'src/google-user/model/google.entity';
-import { GithubUser } from 'src/github-user/model/github.entity';
 import { File } from 'src/avatar/model/avatar.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Chats]),
     TypeOrmModule.forFeature([User]),
-    TypeOrmModule.forFeature([GoogleUser]),
-    TypeOrmModule.forFeature([GithubUser]),
     TypeOrmModule.forFeature([File]),
     ChatsModule,
     AuthModule,
@@ -31,8 +25,6 @@ import { File } from 'src/avatar/model/avatar.entity';
     AuthService,
     ChatsService,
     UserService,
-    GoogleUserService,
-    GithubUserService,
     AvatarService,
     JwtService,
   ],
