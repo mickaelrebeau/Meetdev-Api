2934c44a91b684308f1f0e93b282268f07837e49
Mickael Rebeau <rebeau.mickael@gmail.com>
2024-02-24 13:28:05 +0100

    🛠️ build: update chat table

--- src/chats/chats.module.ts
+++ src/chats/chats.module.ts
@@ -5,26 +5,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthService } from 'src/auth/auth.service';
 import { ChatsService } from './chats.service';
 import { UserService } from 'src/user/user.service';
-import { AvatarService } from 'src/avatar/avatar.service';
 import { JwtService } from '@nestjs/jwt';
 import { User } from 'src/user/model/user.entity';
-import { File } from 'src/avatar/model/avatar.entity';
 import { ChatGateway } from 'src/gateway/gateway.gateway';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Chats]),
     TypeOrmModule.forFeature([User]),
-    TypeOrmModule.forFeature([File]),
   ],
-  providers: [
-    AuthService,
-    ChatsService,
-    UserService,
-    AvatarService,
-    JwtService,
-    ChatGateway,
-  ],
+  providers: [AuthService, ChatsService, UserService, JwtService, ChatGateway],
   controllers: [ChatsController],
 })
 export class ChatsModule {}
